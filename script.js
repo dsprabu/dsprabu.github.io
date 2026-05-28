0831ccffb70d9634f6e42cc2b9179e844f0feca2
dsprabu <sindhuprabu.dhandapani@gmail.com>
2026-05-28 21:01:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+});
+
 document.addEventListener('contextmenu', function(e){
   e.preventDefault();
   alert('Content protection enabled.');

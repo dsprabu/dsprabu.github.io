c04702b2ae990006504eafaa3169062faa1b3266
dsprabu <sindhuprabu.dhandapani@gmail.com>
2026-05-29 15:58:21 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-document.addEventListener('contextmenu',e=>e.preventDefault());
+document.addEventListener('contextmenu', function(e){
+  e.preventDefault();
+  alert('Content protection enabled.');
+});
+
+document.querySelectorAll('img').forEach(img => {
+  img.addEventListener('dragstart', e => e.preventDefault());
+});
